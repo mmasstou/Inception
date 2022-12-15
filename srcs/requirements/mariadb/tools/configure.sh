9e70060dc05fcef73845712b4a897456630ec32a
mohamed MASSTOUR <mmasstou@student.1337.ma>
2022-12-15 22:13:34 +0100

    mariadb is configurad suc

---
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+fi
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    # init database
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm >/dev/null
+
+
+    # https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+    cat <<EOF > "mktemp"
+USE mysql;
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+
+CREATE DATABASE $DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$DATABASE_USR'@'%' IDENTIFIED by '$DATABASE_PWD';
+GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USR'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+    # run init.sql
+    /usr/bin/mysqld --user=mysql --bootstrap < "mktemp"
+    # rm -f  "mktemp"
+fi
+
+tail -f /dev/null
